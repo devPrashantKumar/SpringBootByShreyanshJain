ea250982215651ecc4b1e46476523828c180c98e
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-03-20 17:02:12 +0530

    Simple RestAPI using Servlet

---
+++ SimpleRestAPIUsingServlateProject/src/main/java/HelloServlet.java
@@ -0,0 +1,27 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Serial;
+
+// Define the servlet URL mapping
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String jsonResponse = "{ \"message\": \"Hello, World from Java 21!\" }";
+        try (PrintWriter out = response.getWriter()) {
+            out.write(jsonResponse);
+        }
+    }
+}
