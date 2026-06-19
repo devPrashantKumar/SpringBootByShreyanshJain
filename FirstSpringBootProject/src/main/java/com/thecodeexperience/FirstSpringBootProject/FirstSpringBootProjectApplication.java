fc72578810af32117854a72773541537373e898e
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-06-19 16:59:48 +0530

    commandLineRunner changes

--- FirstSpringBootProject/src/main/java/com/thecodeexperience/FirstSpringBootProject/FirstSpringBootProjectApplication.java
+++ FirstSpringBootProject/src/main/java/com/thecodeexperience/FirstSpringBootProject/FirstSpringBootProjectApplication.java
@@ -3,10 +3,6 @@ package com.thecodeexperience.FirstSpringBootProject;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
